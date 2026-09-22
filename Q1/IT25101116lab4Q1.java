fb15e1cdc3b30bb139941766b41dc879a2e62c44
it25101116 <it25101116@my.sliit.lk>
2026-09-22 20:03:25 +0530

    Add files via upload

---
+++ Q1/IT25101116lab4Q1.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+    public class IT26101614lab4Q1{
+	  public static void main(String[] args){
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("enter the number: ");
+		int number = input.nextInt();
+		
+		if(number>0){
+		    System.out.println("The number is: Positive ");	
+		}
+
+		else if(number<0){
+			System.out.println("The number is: Negative ");
+		}
+
+		else{
+			System.out.println("The number is Zero  ");
+			
+		}
+	  }
+	}	  
